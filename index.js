da1338a9ddd8e402545384e546ca4cfc138319fd
ogparra <ogparra.15@gmail.com>
2017-07-09 16:14:02 -0400

    My first commit

---
+++ index.js
@@ -0,0 +1,36 @@
+
+
+/*
+Add
+Sub
+Div 
+Mul
+*/
+
+
+function addition(){
+
+}
+
+function sub(){
+
+}
+
+function mul(){
+
+}
+
+function div(){
+
+}
+
+
+
+function controller(){
+    var value1 = document.getElementById("input-ele").value;
+    var value2 = document.getElementById("input-ele2").value;
+    var oper = document.getElementById("opertation").value;
+    
+    
+  
+  }
